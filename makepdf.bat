3dc3e5ce59a74ecad70a0a677aed721b036c95a9
射落白羊 <517003047@qq.com>
2014-10-22 17:09:47 +0800

    综述

--- makepdf.bat
+++ makepdf.bat
@@ -1,7 +1,7 @@
 del *.aux *.lo? *.toc *.ind *.inx *.gls *.glo *.idx *.ilg *.out *.bak *.bbl *.brf *.blg *.dvi data\*.aux
-xelatex thesis
-rem makeindex -s nomencl.ist -o thesis.nls thesis.nlo
-bibtex thesis
-bibtex thesis
-xelatex thesis
-xelatex thesis
+REM xelatex thesis
+REM rem makeindex -s nomencl.ist -o thesis.nls thesis.nlo
+REM bibtex thesis
+REM bibtex thesis
+REM xelatex thesis
+REM xelatex thesis
