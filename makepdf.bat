882c51457b13cb97391635f44a52e8cd26db025f
射落白羊 <517003047@qq.com>
2014-11-04 12:59:48 +0800

    半成品

--- makepdf.bat
+++ makepdf.bat
@@ -1,4 +1,4 @@
-del *.aux *.lo? *.toc *.ind *.inx *.gls *.glo *.idx *.ilg *.out *.bak *.bbl *.brf *.blg *.dvi data\*.aux
+del *.aux *.lo? *.toc *.ind *.inx *.gls *.glo *.idx *.ilg *.out *.bak *.bbl *.brf *.blg *.dvi data\*.aux data\*.bak data\*.sav
 REM xelatex thesis
 REM rem makeindex -s nomencl.ist -o thesis.nls thesis.nlo
 REM bibtex thesis
